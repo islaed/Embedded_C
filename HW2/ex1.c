9f97dbc29553e7688a7a7d87f2d9d9decd3c17cf
islaed <2005ilya5002@gmail.com>
2025-12-05 00:32:59 +0700

    первое, второе задание

--- HW2/ex1.c
+++ HW2/ex1.c
@@ -1,1 +1,22 @@
 // Вывести квадратную матрицу по заданному N.
+
+#include <stdio.h>
+
+#define N 3
+
+int main()
+{
+    int a[N][N];
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            a[i][j] = j + 1 + (i * N);
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
--- HW2/ex2.c
+++ HW2/ex2.c
@@ -1,1 +1,27 @@
 // Вывести заданный массив размером N в обратном порядке.
+
+#include <stdio.h>
+
+#define N 7
+
+int main()
+{
+    int a[N];
+    int b[N];
+
+    for(int i = 0; i < N; i++)
+    {
+        a[i] = i + 1;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    for(int i = 0; i < N; i++)
+    {
+        b[i] = a[N - 1 - i];
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
