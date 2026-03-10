bf966d22688678b3fba7c538c86613324e61980b
Ilya <2005ilya5002@gmail.com>
2026-03-11 01:12:19 +0700

    7 дз

---
+++ HW7/div.c
@@ -0,0 +1,5 @@
+float div(int first_num, int second_num)
+{
+    if(second_num == 0) return 0;
+    else return (float)first_num / second_num;
+}
---
+++ HW7/sub.c
@@ -0,0 +1,4 @@
+int sub(int first_num, int second_num)
+{
+    return first_num - second_num;
+}
