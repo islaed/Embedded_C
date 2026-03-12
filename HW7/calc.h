0ea3941c4bbd640ae34712f143b1fa03025a65e1
Ilya <2005ilya5002@gmail.com>
2026-03-12 20:40:13 +0700

    7 дз

---
+++ HW7/calc.h
@@ -0,0 +1,9 @@
+#ifndef CALC_H
+#define CALC_H
+
+int add(int first_num, int second_num);
+int sub(int first_num, int second_num);
+int mul(int first_num, int second_num);
+float div(int first_num, int second_num);
+
+#endif
