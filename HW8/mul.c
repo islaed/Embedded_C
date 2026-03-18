5c9cd7043f0a3981f2386974eaa18781b6e0a527
Ilya <2005ilya5002@gmail.com>
2026-03-18 18:29:34 +0700

    дз 8 и Makefile

---
+++ HW8/mul.c
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+int mul(int first_num, int second_num)
+{
+    return first_num * second_num;
+}
---
+++ HW8/sub.c
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+int sub(int first_num, int second_num)
+{
+    return first_num - second_num;
+}
